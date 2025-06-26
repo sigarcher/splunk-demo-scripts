db6a7ff6b5384862f1b8f4ca157a1ad72646203d
sigarcher <113287654+sigarcher@users.noreply.github.com>
2025-06-26 13:05:00 +1000

    Update setup-lvm.sh

--- setup-lvm.sh
+++ setup-lvm.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# LVM Configuration for Splunk Demo Environment
+# Post-install LVM configuration
+
+# Prepare partition 3 for LVM
+wipefs -a /dev/nvme0n1p3
+partprobe /dev/nvme0n1
 
 # Create physical volume
 pvcreate /dev/nvme0n1p3
@@ -9,30 +13,47 @@ vgcreate ubuntu-vg /dev/nvme0n1p3
 
 # Create logical volumes
 lvcreate -L 50G -n root ubuntu-vg
-lvcreate -L 50G -n home ubuntu-vg
-lvcreate -L 200G -n splunk ubuntu-vg
+lvcreate -L 30G -n home ubuntu-vg
 lvcreate -L 150G -n docker ubuntu-vg
 lvcreate -L 100G -n libvirt ubuntu-vg
-lvcreate -L 400G -n thin-pool ubuntu-vg
 
 # Format filesystems
 mkfs.ext4 /dev/ubuntu-vg/root
 mkfs.ext4 /dev/ubuntu-vg/home
-mkfs.ext4 /dev/ubuntu-vg/splunk
 mkfs.ext4 /dev/ubuntu-vg/docker
 mkfs.ext4 /dev/ubuntu-vg/libvirt
 
-# Create mount points
-mkdir -p /opt/splunk /var/lib/docker /var/lib/libvirt
+# Mount new volumes
+mount /dev/ubuntu-vg/root /mnt
+mkdir -p /mnt/{home,var/lib/docker,var/lib/libvirt}
+mount /dev/ubuntu-vg/home /mnt/home
+mount /dev/ubuntu-vg/docker /mnt/var/lib/docker
+mount /dev/ubuntu-vg/libvirt /mnt/var/lib/libvirt
 
-# Configure fstab
-cat >> /etc/fstab << EOF
-/dev/ubuntu-vg/root / ext4 defaults 0 1
-/dev/ubuntu-vg/home /home ext4 defaults 0 2
-/dev/ubuntu-vg/splunk /opt/splunk ext4 defaults 0 2
-/dev/ubuntu-vg/docker /var/lib/docker ext4 defaults 0 2
-/dev/ubuntu-vg/libvirt /var/lib/libvirt ext4 defaults 0 2
+# Migrate OS to LVM
+rsync -aAX / /mnt --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*"}
+mount --bind /dev /mnt/dev
+mount --bind /proc /mnt/proc
+mount --bind /sys /mnt/sys
+mount --bind /run /mnt/run
+
+# Chroot and update bootloader
+chroot /mnt <<EOF
+mount /dev/nvme0n1p2 /boot
+mount /dev/nvme0n1p1 /boot/efi
+update-initramfs -u
+update-grub
 EOF
 
-# Mount all filesystems
-mount -a
+# Update fstab
+cat > /mnt/etc/fstab <<EOF
+/dev/ubuntu-vg/root    /         ext4    defaults        0 1
+/dev/ubuntu-vg/home    /home     ext4    defaults        0 2
+/dev/ubuntu-vg/docker  /var/lib/docker  ext4 defaults   0 2
+/dev/ubuntu-vg/libvirt /var/lib/libvirt ext4 defaults   0 2
+/dev/nvme0n1p2         /boot     ext4    defaults        0 2
+/dev/nvme0n1p1         /boot/efi vfat    umask=0077     0 1
+EOF
+
+# Reboot into new system
+reboot
